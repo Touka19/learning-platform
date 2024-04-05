f7ffa0574935c8c3505f5d16e80e4e37a0265632
Lylia <71930916+dishapatel010@users.noreply.github.com>
2024-04-05 22:17:51 +0530

    Update teacher.ts

--- lib/teacher.ts
+++ lib/teacher.ts
@@ -1,4 +1,4 @@
 export const isTeacher = (userId?: string | null) => {
   const teacherIds = process.env.NEXT_PUBLIC_TEACHER_ID?.split(",");
-  return teacherIds?.includes(userId);
+  return userId && teacherIds?.includes(userId);
 };
