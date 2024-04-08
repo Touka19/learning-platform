53ce575b101851b80c21b2d8e49ac1266c168be0
Lylia <71930916+dishapatel010@users.noreply.github.com>
2024-04-08 21:19:57 +0530

    fix cache

--- app/api/folder/route.ts
+++ app/api/folder/route.ts
@@ -3,6 +3,8 @@ import { NextResponse } from "next/server";
 
 import { db } from "@/lib/db";
 
+export const dynamic = 'force-dynamic';
+
 export async function GET(req: any, res: NextResponse) {
   try {
     const folders: any[] = await db.folderId.findMany()
